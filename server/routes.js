139808c7bd09efe8b5b054848cc842e9621a7549
tiger1028 <gentleman.tiger1028@gmail.com>
2014-11-23 01:11:30 -0600

    Added get, post, delete resource methods for catalog.

--- server/routes.js
+++ server/routes.js
@@ -1,17 +1,32 @@
 'use strict';
 /**
- * Server routes.
- * Handle things like API calls and authentication. Route all other requests to frontend.
+ * Catalog routes.
  */
 var express = require('express');
 var router = express.Router();
 var Catalog = require('./models/catalog');
+var objectid = require('objectid');
 
 module.exports = function(app) {
     /**
-     * Return all catalog items.
+     * ID parameter handler.
+     */
+    router.param('resourceId', function(req, res, next, id) {
+        if (objectid.isValid(id)) {
+            req.resourceId = id;
+            next();
+        } else {
+            res.status(400).send('Invalid ID specifier: ' + id);
+        }
+    });
+
+    /**
+     * Catalog list resources.
      */
     router.route('/v1/catalog')
+        /**
+         * Get all catalog items.
+         */
         .get(function(req, res) {
             Catalog.CatalogItem.find({}, function(err, catalogItems) {
                 // Send error if one occured.
@@ -20,15 +35,29 @@ module.exports = function(app) {
                 // Return all catalog items.
                 res.json(catalogItems);
             });
+        })
+
+        /**
+         * Create new catalog items.
+         */
+        .post(function(req, res) {
+            new Catalog.CatalogItem(req.body).save(function(err, product, numberAffected) {
+                if (err) throw err;
+                console.log(product);
+                res.status(200).json(product);
+            });
         });
 
     /**
-     * TODO(TheDodd): add route for create.
+     * Individual catalog resources.
      */
-
-    /**
-     * TODO(TheDodd): add route for delete.
-     */
+    router.route('/v1/catalog/:resourceId')
+        .delete(function(req, res) {
+            Catalog.CatalogItem.findByIdAndRemove(req.resourceId, function(product) {
+                // Yea, this is jank, but product is not being properly populated.
+                res.status(200).send('OK');
+            });
+        });
 
     app.use('/api', router);
 };
