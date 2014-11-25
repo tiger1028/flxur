0dec0687d083660ac273cd4d7d21830e68c7c3f7
tiger1028 <gentleman.tiger1028@gmail.com>
2014-11-24 21:58:37 -0600

    Yup ... React is the tits.

---
+++ app/js/components/__tests__/test-addtocart.js
@@ -0,0 +1,27 @@
+'use strict';
+/**
+ * Tests for app/js/components/addtocart.js.
+ */
+
+describe('addtocart', function() {
+    it('is a <button/> with text "Add to Cart" and onClick listener', function() {
+        jest.dontMock('../addtocart.js');
+        var React = require('react/addons');
+        var Actions = require('../../actions/actions.js');
+        var AddToCart = require('../addtocart.js');
+        var TestUtils = React.addons.TestUtils;
+
+        // Render component.
+        var item = {title: 'Test Item', cost: 1};
+        var dom = TestUtils.renderIntoDocument(<AddToCart item={item}/>);
+        var component = TestUtils.findRenderedComponentWithType(dom, AddToCart).getDOMNode();
+
+        // Assert that component appears as expected.
+        expect(component.textContent).toEqual('Add to Cart');
+        expect(component.tagName).toEqual('BUTTON');
+
+        // Simulate click and assert.
+        TestUtils.Simulate.click(component);
+        expect(Actions.addItem).lastCalledWith(item);
+    });
+});
